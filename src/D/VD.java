f5b1f93767587e4781245b46f070ca7c9a24d243
pliasokhan <domato19@gmail.com>
2022-02-03 20:41:00 -0700

    Commit 3

--- src/D/VD.java
+++ src/D/VD.java
@@ -4,10 +4,7 @@ public class VD {
 
     //hello
 
-    public static void main(String[] args) {
-        System.out.println("Hello everybody");
-
-        System.out.println("We are here");
+  // hello hello
 
         // doubt
     }
